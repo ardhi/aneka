75e84a0ee391ee985183b27ecd31e8b5d61a1038
Ardhi Lukianto <ardhi@lukianto.com>
2022-01-06 19:27:12 +0700

    Terminate defaults to trie

--- src/misc/fatal.js
+++ src/misc/fatal.js
@@ -1,8 +1,10 @@
 const print = require('./print')
 const { get } = require('lodash')
+const isSet = require('./is-set')
 
-module.exports = msg => {
+module.exports = function (msg, terminate) {
+  if (!isSet(terminate)) terminate = true
   const debug = get(process, 'env.DEBUG', false)
   if (msg instanceof Error) msg = msg.message
-  print(msg, true, debug ? 'trace' : 'error')
+  print(msg, terminate, debug ? 'trace' : 'error')
 }
