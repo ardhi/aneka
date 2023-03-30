a2ce5bc5dcf194c997ceb8575016fc6d2fc91f8a
Ardhi Lukianto <ardhi@lukianto.com>
2023-03-30 21:09:53 +0700

    Updates

--- src/misc/delay.js
+++ src/misc/delay.js
@@ -1,7 +1,18 @@
-module.exports = function (ms = 1000) {
+const wt = require('worker-timers')
+
+module.exports = function (ms = 1000, useWorker) {
   return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve()
-    }, ms)
+    let timer
+    if (useWorker) {
+      timer = wt.setTimeout(() => {
+        if (timer) wt.clearTimeout(timer)
+        resolve()
+      }, ms)
+    } else {
+      timer = setTimeout(() => {
+        if (timer) clearTimeout(timer)
+        resolve()
+      }, ms)
+    }
   })
 }
